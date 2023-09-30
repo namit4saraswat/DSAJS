cef1292a9662b6151cabbcb1c96f20a77e2e61e5
notiffy <inotiffy@gmail.com>
2023-09-30 06:50:47 +0530

    set Matrix Zeroes

--- Matrix/Medium/setMatrixZeroes.js
+++ Matrix/Medium/setMatrixZeroes.js
@@ -3,6 +3,7 @@
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 
+
 var printMatrix = function(matrix){
     const n= matrix.length;
     const m= matrix[0].length;
@@ -16,42 +17,72 @@ var printMatrix = function(matrix){
     console.log(ans);
 }
 
-var setZeroes = function(matrix) {
-    let update = function(matrix,i,j){
-        for(let k=0;k<matrix[i].length;k++){
-            if(matrix[i][k] === 0){
-                continue;
-            }else{
-                matrix[i][k] =  'a';
-            }
-        }
-        for(let k=0;k<matrix.length;k++){
-            if(matrix[k][j] === 0){
-                continue;
-            }else{
-                matrix[k][j] =  'a';
-            }
-        }
-    }
+//Solution1
+// var setZeroes = function(matrix) {
+//     let update = function(matrix,i,j){
+//         for(let k=0;k<matrix[i].length;k++){
+//             if(matrix[i][k] === 0){
+//                 continue;
+//             }else{
+//                 matrix[i][k] =  'a';
+//             }
+//         }
+//         for(let k=0;k<matrix.length;k++){
+//             if(matrix[k][j] === 0){
+//                 continue;
+//             }else{
+//                 matrix[k][j] =  'a';
+//             }
+//         }
+//     }
     
-    for(let i=0;i<matrix.length;i++){
-        for(let j=0;j<matrix[i].length;j++){
-            if(matrix[i][j] === 0){
-               update(matrix,i,j)
+//     for(let i=0;i<matrix.length;i++){
+//         for(let j=0;j<matrix[i].length;j++){
+//             if(matrix[i][j] === 0){
+//                update(matrix,i,j)
+//             }
+//         }
+//     }
+
+//     for(let i=0;i<matrix.length;i++){
+//         for(let j=0;j<matrix[i].length;j++){
+//             if(matrix[i][j] === 'a'){
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+
+//     printMatrix(matrix);
+// };
+
+//Solution2
+
+var setZeroes = function (matrix) {
+    let arr1 = [];
+    let arr2 = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                arr1.push(i);
+                arr2.push(j);
             }
         }
     }
 
-    for(let i=0;i<matrix.length;i++){
-        for(let j=0;j<matrix[i].length;j++){
-            if(matrix[i][j] === 'a'){
-                matrix[i][j] = 0;
-            }
+    for(let k=0;k<arr1.length;k++){
+        for(let i=0;i<matrix[0].length;i++){
+            matrix[arr1[k]][i] = 0;
+        } 
+    }
+
+    for(let k=0;k<arr2.length;k++){
+        for(let i=0;i<matrix.length;i++){
+            matrix[i][arr2[k]]= 0;
         }
     }
 
-    printMatrix(matrix);
-};
+    printMatrix(matrix)
+}
 
-
-setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]])
+setZeroes([[1,0]])
