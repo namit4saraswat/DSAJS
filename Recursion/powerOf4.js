49ddddf87ceaf3cb9373ecdd86b060b34dee7e76
notiffy <inotiffy@gmail.com>
2023-11-24 07:10:06 +0530

    power of 2 3 4

--- Recursion/powerOf4.js
+++ Recursion/powerOf4.js
@@ -7,4 +7,15 @@ var isPowerOfFour = function(n) {
     
 };
 
-console.log(isPowerOfFour(1))
+var isPowerOfTwo = function(n) {
+    return n > 0 && (Math.log2(n) % 2 === 0 ||  Math.log2(n) % 2 === 1);
+    
+};
+
+var isPowerOfThree = function(n) {
+    let num = (Math.log(n)/Math.log(3)).toFixed(9);
+    return n > 0 && (num % 2 === 0 ||  num % 2 === 1);
+    
+};
+
+console.log(isPowerOfThree(531440))
