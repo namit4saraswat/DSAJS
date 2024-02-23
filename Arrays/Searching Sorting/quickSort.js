76d1a935f9896379e8e8777539960e5216a47eb1
notiffy <inotiffy@gmail.com>
2024-02-23 08:40:47 +0530

    quick sort

--- Arrays/Searching Sorting/quickSort.js
+++ Arrays/Searching Sorting/quickSort.js
@@ -1,42 +1,33 @@
-var partition = function(nums,l,h){
-    console.log(l)
-    let pivot = nums[l]
-    let i = l
-    let j = h
-    while(i<j){
-        do{
-            i++
-        }while(nums[i]<=pivot)
+var swap = function(X,a,b){
+    let temp = X[a]
+    X[a] = X[b]
+    X[b] = temp
+    return X
+}
 
-        do{
-            j--
-        }while(nums[j]>=pivot)
-
-        if(i<j){
-            let temp = nums[i]
-            nums[i] = nums[j]
-            nums[j] = temp
+var partition = function(X,l,r){
+    let pIndex = r
+    let j=l-1
+    for(let i=l;i<=r;i++){
+        if(X[pIndex]>X[i]){
+            j++
+            X = swap(X,j,i)
         }
-
-        
     }
-
-    let temp = nums[l]
-    nums[l] = nums[j]
-    nums[j] = temp
-    return j
+    X=swap(X,j+1,r)
+    return j+1
 }
 
-var quickSort = function(nums,l,h){
-    let j = partition(nums, l, h)
-    quickSort(l,j)
-    quickSort(j+1,h)
+var quickSort = function(X, l,r){
+    if(l>=r){
+        return
+    }
+    let pIndex = partition(X,l,r)
+    quickSort(X,l,pIndex-1)
+    quickSort(X,pIndex+1,r)
 
+    return X
 }
 
-let nums= [10,16,8,12,15,6,3,9,5]
-let l=0
-let h= nums.length
-nums[h] = Infinity
-quickSort(nums,l,h)
-console.log(nums)
+
+console.log(quickSort([12,54,6,2,7,14,15,16],0,7))
