a986094a0984804a1abb4c4e43fa48041c61b4d1
notiffy <inotiffy@gmail.com>
2024-03-26 08:54:10 +0530

    traversal BFS

--- Graph/implement.js
+++ Graph/implement.js
@@ -12,6 +12,27 @@ class Graph {
         this.adjacencyList[v].push(u)
     }
 
+    bfsTraversal(source){
+        let queued = []
+        let visited = new Array(Object.keys(this.adjacencyList).length).fill(false);
+
+        visited[source] = true
+        queued.push(source)
+
+        while(queued.length!=0){
+            const currentNode =  queued.shift()
+            console.log(currentNode)
+
+            for(const neighbor of this.adjacencyList[currentNode]){
+                if(!visited[neighbor]){
+                    visited[neighbor]=true
+                    queued.push(neighbor)
+                }
+            }
+        }
+       
+    }
+
     printGraph(){
         console.log(this.adjacencyList)
     }
@@ -22,8 +43,13 @@ obj.addVertex(0)
 obj.addVertex(1)
 obj.addVertex(2)
 obj.addVertex(3)
+obj.addVertex(4)
 obj.addEdge(0,1)
-obj.addEdge(1,2)
 obj.addEdge(0,2)
+obj.addEdge(1,3)
+obj.addEdge(1,2)
 obj.addEdge(2,3)
+obj.addEdge(2,4)
+obj.addEdge(3,4)
 obj.printGraph()
+obj.bfsTraversal(0)
