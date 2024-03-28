c47604f705329309301437164ff4c8c96a97dffa
notiffy <inotiffy@gmail.com>
2024-03-28 07:47:51 +0530

    3Sum

--- TwoPointers/Medium/3sum.js
+++ TwoPointers/Medium/3sum.js
@@ -1,27 +1,32 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var threeSum = function(nums) {
-    nums.sort((a, b) => a - b);
-    const result = [];
-    
-    for(let i = 0; i < nums.length; i++) {
-        let low = i+1, high = nums.length-1, sum = 0;
-
-        while(low<high){
-            sum = nums[low] + nums[high] + nums[i];
-
-            if(sum===0){
-                result.push([nums[i], nums[low], nums[high]]);
+var threeSum = function (nums){
+    let ans = []
+    nums = nums.sort(function(a,b){return a-b})
+    for(let i=0;i<nums.length-2;i++){
+        let lo = i+1
+        let hi = nums.length-1
+        if((nums[i] === nums[i-1]) && i>0){
+            continue
+        }
+        while(lo<hi){
+            let current = nums[lo] + nums[hi] + nums[i] 
+            if(current === 0){
+                ans.push([nums[i],nums[lo],nums[hi]])
+                lo++
+                hi--
+                while(lo<hi && nums[lo-1]===nums[lo] && nums[hi+1]=== nums[hi]){
+                    lo++
+                    hi--
+                }
+                }else if(0>current){
+                lo++
+                }else{
+                    hi--
+                }
                 
-            }
-
         }
     }
-
-    return number;
     
-};
+    return ans
+}
 
-console.log(threeSum([-1,0,1,2,-1,-4]));
+console.log(threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
