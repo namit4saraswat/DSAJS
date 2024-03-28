2ae70a747e7f7523f5003fe3d5c28f6f3e49ca3b
notiffy <inotiffy@gmail.com>
2024-03-28 18:49:49 +0530

    add Two number linked list

--- LinkedList/addTwoNumber.js
+++ LinkedList/addTwoNumber.js
@@ -15,30 +15,29 @@ Explanation: 342 + 465 = 807.
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
-    let num1 = 0;
-    let count = 1;
-    let num2 = 0;
-    while(l1){
-        num1 = l1.val * (count) + num1
-        count = count * 10
-        l1 = l1.next;
+var addTwoNumbers = function (l1, l2) {
+    let carryOver = 0
+    let unitSum = 0
+    let head3 = new ListNode()
+    let head = head3
+    head = head3
+    while (l1 || l2) {
+        if (l1) unitSum = l1.data + carryOver
+        if (l2) unitSum = unitSum + l2.data
+        if (unitSum > 9) {
+            unitSum = unitSum - 10
+            carryOver = 1
+        } else {
+            carryOver = 0
+        }
+        head3.next = new ListNode(unitSum)
+        head3 = head3.next
+        if (l1) l1 = l1.next
+        if (l2) l2 = l2.next
     }
-    count =1;
-    while(l2){
-        num2 = l2.val * (count) + num2
-        count = count * 10
-        l2 = l2.next;
+    if (carryOver === 1) {
+        head3.next = new Node(carryOver)
     }
-    let sum = num1 + num2;
-    sum = sum.toString();
-    let len = sum.length;
-    let i=0;
-    let head = null;
-    while(len>0){
-        head = new Node(parseInt(sum[i]), head)
-        len--;
-        i++;
-    }
+    head = head.next
     return head
 };
