a0f1efe26a7d47b14bfb291ff78cdccca23f3c8b
notiffy <inotiffy@gmail.com>
2024-04-06 07:48:29 +0530

    merge Sorted Element

--- Arrays/Easy/mergeSortedArray.js
+++ Arrays/Easy/mergeSortedArray.js
@@ -5,33 +5,18 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-//nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3 => [1,2,2,3,5,6]
-var merge = function (nums1, m, nums2, n) {
-    let i = 1;
-    let j = 1;
-    let count = 1;
-    if (nums1.length === 0) {
-        return nums1 = nums2;
-    }
-    
-    while(i<=m && j<=n){
-        if(nums1[m-i]<nums2[n-j]){
-            nums1[m+n-count] = nums2[n-j]
-            j++
+var merge = function(nums1, m, nums2, n) {
+    while(n>0){
+        if(nums1[m-1]>=nums2[n-1]){
+            nums1[m+n-1]= nums1[m-1]
+            m--
         }else{
-            nums1[m+n-count] = nums1[m-i]
-            i++
-        }
-        count++;
+            nums1[m+n-1]= nums2[n-1]
+            n--
+        }        
     }
 
-    while(j<=n){
-        nums1[m+n-count] = nums2[n-j] 
-        j++
-        count++
-    }
-
-    return nums1;
+    return nums1
 };
 
-console.log("ans is " + merge([7,8,9,0,0,0], 3, [2,5,6], 3));
+console.log(merge([1],1,[],0))
