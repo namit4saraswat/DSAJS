470250450fc34b8694e0d88a92dda3ad76ab050f
notiffy <inotiffy@gmail.com>
2024-04-07 23:30:51 +0530

    most water container

--- Arrays/Medium/mostWaterCont.js
+++ Arrays/Medium/mostWaterCont.js
@@ -3,24 +3,18 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let water = 0
-    let left = 0
-    let right = n-1
-    let leftMax = height[left]
-    let area =0
-    let maxArea = 0
-    while(left<right){
-        if(leftMax<height[left]){
-            leftMax = height[left]
-        }
-        if(height[left+1]<leftMax){
-            area = area + Math.min(height[left], height[left+1])
+    let l=0
+    let r=height.length-1
+    let lmax=0
+    let rmax=0
+    let area=0
+    while(l<r){
+        area=Math.max(area,(Math.min(height[l],height[r])*(r-l)))
+        if(height[l]>height[r]){
+            r--
         }else{
-            area = Math.min(height[left], height[left+1])
+            l++
         }
-        if(area>maxArea){
-            maxArea = area
-        }
-        left++
-    }
+    }   
+    return area
 };
