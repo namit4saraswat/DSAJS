a21bf5f6d6d399d790def4722eef02d659a54c80
notiffy <inotiffy@gmail.com>
2024-04-09 19:52:38 +0530

    updated topological sorting

--- Graph/implement.js
+++ Graph/implement.js
@@ -12,25 +12,26 @@ class Graph {
         this.adjacencyList[v].push(u)
     }
 
+    addEdgeDirected(u, v) {
+        this.adjacencyList[u].push(v)
+    }
+
     bfsTraversal(source) {
         let queued = []
         let visited = new Array(Object.keys(this.adjacencyList).length).fill(false);
 
         visited[source] = true
         queued.push(source)
-
-        while (queued.length != 0) {
-            const currentNode = queued.shift()
+        while(queued.length!=0){
+            let currentNode = queued.shift()
             console.log(currentNode)
-
-            for (const neighbor of this.adjacencyList[currentNode]) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true
-                    queued.push(neighbor)
+            for(let nums of this.adjacencyList[currentNode]){
+                if(!visited[nums]){
+                    visited[nums]=true
+                    queued.push(nums)
                 }
             }
-        }
-
+        }    
     }
 
     dfsUtil(source, visited) {
@@ -50,46 +51,87 @@ class Graph {
         this.dfsUtil(source, visited)
     }
 
-    isCyclicUtil(source, visited, parent) {
-
-        visited[source] = true
-
-        for (let i = 0; i < this.adjacencyList[source].length; i++) {
-            if(!visited[this.adjacencyList[source][i]]){
-                if(this.isCyclicUtil(this.adjacencyList[source][i],visited, source)){
+    isCyclicUtil(source,visited,parent){
+        
+        visited[source] =true
+        for (let i of this.adjacencyList[source].values()) {
+            
+            let n = i
+            if (!visited[n]) {
+                if(this.isCyclicUtil(n, visited,source) ===true){
                     return true
                 }
             }else{
-                if(parent != this.graph[v][i]){
-                    return true;
+                if(source!=parent){
+                    return true
                 }
             }
         }
+
+        return false
+
     }
 
-    isCyclic() {
-        let visited = new Array((this.adjacencyList).length).fill(false);
-        for (let i = 0; i < this.adjacencyList.length; i++) {
-            if (visited[i] == false) {
-                this.isCyclicUtil()
+    isCyclic(){
+        let visited = new Array(Object.keys(this.adjacencyList).length).fill(false);
+        for(let i = 0; i < visited.length; i++){
+            // Don't recur for u if it
+            // is already visited
+            if(visited[i] == false){
+                if(this.isCyclicUtil(i, visited, -1) === true){
+                    return true;
+                }
             }
+                 
         }
     }
-        printGraph(){
-            console.log(this.adjacencyList)
+
+    topologicalSortUtil(source, visited, stack){
+        visited[source]=true
+
+        for(let num of this.adjacencyList[source].values()){
+            if(!visited[num]){
+                this.topologicalSortUtil(num,visited,stack)
+            }
         }
+
+        stack.push(source)
+    }
+
+    topologicalSort(source){
+        let visited = new Array(Object.keys(this.adjacencyList).length).fill(false);
+        let stack = []
+        for(let i=0;i<visited.length;i++){
+            if(!visited[i]){
+                this.topologicalSortUtil(i,visited, stack)
+            }
+        }
+
+        console.log("Topological sorting of the graph: ");
+        while (stack.length > 0) {
+        console.log(stack.pop() + " ");
+    }
+    }
+    
+    printGraph(){
+        console.log(this.adjacencyList)
+    }
     
     }
 
 let obj = new Graph()
-obj.addVertex(4)
+obj.addVertex(0)
 obj.addVertex(1)
 obj.addVertex(2)
 obj.addVertex(3)
-obj.addVertex(0)
-obj.addEdge(0, 1)
-obj.addEdge(1, 3)
-obj.addEdge(1, 2)
-obj.addEdge(2, 3)
-obj.printGraph()
-console.log(obj.isCyclic(-1, 0))
+obj.addVertex(4)
+obj.addVertex(5)
+obj.addEdgeDirected(5,1)
+obj.addEdgeDirected(5,2)
+obj.addEdgeDirected(1,3)
+obj.addEdgeDirected(2,3)
+obj.addEdgeDirected(0,4)
+obj.addEdgeDirected(0,3)
+// obj.printGraph()
+// obj.dfsTraversal(5)
+obj.topologicalSort(5)
