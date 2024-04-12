22e2e8aa8e7ecbe250a941452d63597d772f5539
notiffy <inotiffy@gmail.com>
2024-04-12 08:33:54 +0530

    flood fill

--- Graph/FloodFill.js
+++ Graph/FloodFill.js
@@ -5,53 +5,27 @@
  * @param {number} color
  * @return {number[][]}
  */
-
-function isValid(i, j, n, m, image,col) {
-    if (i < n && i >= 0 && j < m && j >= 0 && image[i][j] === col) {
-        return true
-    }
-
-    return false
-}
-
-function floodFillRec(i, j, n, m, image,color,col) {
-    if(image[i][j]===col){
-        image[i][j] = color
-    }
-
-    if (isValid(i + 1, j, n, m, image,col)) {
-        floodFillRec(i + 1, j, n, m,image,color,col)
-    }
-    
-    if (isValid(i - 1, j, n, m, image,col)) {
-        floodFillRec(i - 1, j, n, m,image,color,col)
-    }
-    
-   
-    if (isValid(i, j + 1, n, m, image,col)) {
-        floodFillRec(i, j + 1, n, m, image,color,col)
-    }
-    
-    if (isValid(i, j - 1, n, m, image,col)) {
-        floodFillRec(i, j - 1, n, m, image,color,col)
-    }
-    
-
-}
-
 var floodFill = function(image, sr, sc, color) {
-    let n = image.length
-    let m = image[0].length
-    const col = image[sr][sc]
-    if(col === color){
-        return image
-    }
-    floodFillRec(sr, sc, n, m, image,color,col)
-        
-    
+    let m=image.length
+    let n=image[0].length
+    let r=[0,1,0,-1]
+    let c=[1,0,-1,0]
 
+    var dfs= function(i,j,px){
+        if(i<m && j<n && i>=0 && j>=0 && image[i][j]===px && image[i][j]!=color){
+            for(let p=0;p<r.length;p++){
+                image[i][j]=color
+                dfs(i+r[p],j+c[p],px)
+                    
+                
+            }
+        }
+    }
+
+    dfs(sr,sc,image[sr][sc])
     return image
+
 };
 
 
-console.log(floodFill([[0,0,0],[0,0,0]], 0,0,1))
+console.log(floodFill([[0,0,0],[0,0,0]],0,0,0))
