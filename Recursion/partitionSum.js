443919175a003823cf6b8e43fffc91b8f1faff54
notiffy <inotiffy@gmail.com>
2024-05-02 20:44:38 +0530

    applying dp for partiion sum

--- Recursion/partitionSum.js
+++ Recursion/partitionSum.js
@@ -8,26 +8,25 @@
  * @return {number[][]}
  */
 
-let dp = Array(22).fill(Array(22).fill(-1))
-let p2=0
-let p1=0
-function isSubsetSum(arr,n,p1sum,p2sum){
-    if(n===0){
-        return
-    }
-    
-    if(p1sum===p2sum){
+let dp = Array(200).fill(Array(10000).fill(-1))
+function isSubsetSum(arr,n,sum){
+    if(sum===0){
         return true
     }
-    
-    if(isSubsetSum(arr,n-1,p1sum-arr[n-1],p2sum+arr[n-1])){
-        return true
+    if( n===0){
+        return false
     }
-    
-    if(isSubsetSum(arr,n-1,p1sum,p2sum)){
-        return true
+    if(dp[n-1][sum]!=-1){
+        
+        return dp[n-1][sum]
     }
 
+    if(sum[n-1]>sum){
+        return dp[n-1][sum]= isSubsetSum(arr, n - 1, sum)
+    }else{
+        return dp[n-1][sum] =isSubsetSum(arr,n-1,sum-arr[n-1])  || isSubsetSum(arr,n-1,sum)   
+    }
+    
 }
 
 var canPartition = function(nums) {
@@ -35,11 +34,11 @@ var canPartition = function(nums) {
     for(let i=0;i<nums.length;i++){
         sum=sum+nums[i]
     }
-    if(isSubsetSum(nums,nums.length,sum,0)){
+    if(isSubsetSum(nums,nums.length,sum/2)){
         return true
     }else{
         return false
     }
 };
 
-console.log(canPartition([1,5,11,5]))
+console.log(canPartition([28,4,26,1,36,42,10,32,27]))
