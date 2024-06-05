018e2253a2692c3aa44b9bb8f10ae50510f554e4
namit4saraswat <namit.and.saraswat@gmail.com>
2024-06-05 09:17:59 +0530

    revese linked list

--- LinkedList/reverseLinkedList2.js
+++ LinkedList/reverseLinkedList2.js
@@ -1,43 +1,35 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @param {number} left
- * @param {number} right
- * @return {ListNode}
- */
-var reverseBetween = function(head, left, right) {
-    let current = head
-    let i=1;
-    let leftNode = null;
-    let leftEdge = null
-    while(current){
-        if(i === left -1){
-            leftEdge = current
-        }
-        leftNode = current
-        let prev = current;
-        current = current.next;
-        if(i===left){
-            while(i != right){
-                const nextNode = current.next;
-                current.next=prev;
-                prev= current;
-                current=nextNode;
-                i++
-            }
-            leftNode.next = current
-            if(left === 1){
-                return prev
-            }
-            leftEdge.next = prev
-            return head
-        }
-        i++;
+
+const  {getRootNode, printList} =  require ("./createLLFromArr")
+
+
+
+function reverseLinkedList(root){
+    if(!root.next){
+        return root
     }
-};
+    let curr = root.next
+    let curr2 = curr.next
+    if(!curr2){
+        curr.next = root
+        root.next = null
+        return curr
+    }
+    root.next = null
+    while(curr2){
+        curr.next = root
+        root = curr
+        curr = curr2
+        if(!curr2.next){
+            curr.next = root
+            return curr
+        }
+        curr2 = curr2.next
+        
+    }
+}
+
+const root = getRootNode([1,2])
+let head = reverseLinkedList(root)
+console.log(printList(head))
+
+
