7c772211993a523c17877effd1bdf078eef487a7
namit4saraswat <namit.and.saraswat@gmail.com>
2024-06-14 20:38:53 +0530

    jump game 2

--- Arrays/Medium/jumpGame2.js
+++ Arrays/Medium/jumpGame2.js
@@ -2,29 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
+var jump = function(nums) {
+    let jumps = -1;       
+    let currentEnd = 0;  
+    let farthest = 0;   
 
-
-
-var jump = function(N) {
-    let len = N.length - 1, curr = -1, next = 0, ans = 0
-    for (let i = 0; next < len; i++) {
-        if (i > curr) ans++, curr = next
-        next = Math.max(next, N[i] + i)
+    for(let i=0;i<nums.length;i++){
+        farthest = Math.max(i+nums[i],farthest)
+        if(i === currentEnd){
+            jumps++
+            currentEnd = farthest
+        }
     }
-    return ans
+    return jumps
 };
-// console.log(jump([1,4,1,1,1,4,1,1,1,1,1]));
-console.log(jump([1,4,1,5,1,4,1,1,2,1,1]));
-// console.log(jump([2,3,1,1,4]));
-// console.log(jump([2,1,1,1,1]));
-// console.log(jump([1,2,2,1,3,0,0,3]));// "true" + 
-// console.log(canJump([4,3,2,0,0,8]));//"false" +
-// console.log(canJump( [2,0,1]));//"true" +
-// console.log(canJump( [2,0,1,0]));//"true" +
-// console.log(canJump( [2,0,0,0]));//"false" +
-// console.log(canJump([3,1,0,0]));//"true" + 
-// console.log(canJump( [2,0,0]));//"true" +
-// console.log(canJump( [1,0,1,0]));//"false" +
-// console.log(canJump([1,2,0,1]));//"true" + 
-// console.log(canJump([3,0,8,2,0,0,1]))//"true"
-// console.log(canJump([1,1,0,0]));//false
+
+console.log(jump([2,3,3,0,0,4]))
