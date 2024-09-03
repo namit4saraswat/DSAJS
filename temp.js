3a2927159c841ce8ee4de20cef86858067a9c32d
namit4saraswat <namit.and.saraswat@gmail.com>
2024-09-03 13:04:07 +0530

    magic number using slow and fast

--- temp.js
+++ temp.js
@@ -1,24 +1,31 @@
-const  {getRootNode, printList} =  require ("./LinkedList/createLLFromArr")
-
 /**
- * @param {ListNode} head
+ * @param {number} n
  * @return {boolean}
  */
-var hasCycle = function(head) {
-    let slow=head
-    let fast=head
-    while(fast && fast.next){
-        slow=slow.next
-        fast=fast.next.next
+var isHappy = function(n) {
+    function square(num){
+        let out=0
+        while(num>0){
+            out+=Math.pow(num%10,2)
+            num=Math.floor(num/10)
+        }
+        return out
+    }
+    
+    let slow=n
+    let fast=n
+    while(slow!=1 && fast !=1){
+        
+        slow=square(slow)
+        fast=square(square(fast))
         if(slow===fast){
-            return true
+            return false
         }
     }
 
-    return false
+    return true
+    
+
 };
 
-const root = getRootNode([1,2,3,4,5,6,7,8,9,10])
-
-console.log(hasCycle(root))
-
+console.log(isHappy(19))
