d96bee46964fb00806c4af1a65aa01a4d8772edf
namit4saraswat <namit.and.saraswat@gmail.com>
2024-09-03 14:27:28 +0530

    92 reverse linked list with given index left and right

--- temp.js
+++ temp.js
@@ -1,22 +1,36 @@
 const  {getRootNode, printList} =  require ("./LinkedList/createLLFromArr")
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var reverseList = function(head) {
-    let prev = null
-    let curr = head
-    let ahead = head
-    while(curr){
-        ahead = ahead.next
-        curr.next = prev
-        prev = curr
-        curr = ahead
+
+var reverseBetween = function(head, left, right) {
+    if(head == null || head.next == null){
+        return head
     }
-    return prev
+    let prev = null
+    let curr= head
+    let ahead = head
+    let count = 1
+    last = curr
+    first = ahead
+    while(curr && count <= right){
+        if(count >=left){
+            ahead = ahead.next
+            curr.next = prev
+            prev = curr
+            curr = ahead
+        }else{
+            last = curr
+            prev = curr
+            curr = curr.next
+            ahead = ahead.next
+            first = ahead
+        }
+        count++
+        
+    }
+    last.next = prev
+    first.next = curr
+    return left===1 ? prev : head
 };
 
-
-const root = getRootNode([1,2,3,4,5,6,7,8,9,10])
-let head = reverseList(root)
+const root = getRootNode([5,6])
+let head = reverseBetween(root,1,2)
 console.log(printList(head))
