233366c870d041aec9b8f57604712fe9e1dc1ab9
namit4saraswat <namit.and.saraswat@gmail.com>
2024-09-03 16:37:30 +0530

    347 Top k frequent element

--- temp.js
+++ temp.js
@@ -1,11 +1,35 @@
-var findKthLargest = function(nums, k) {
-    const minPriQue = new MinPriorityQueue();
-    for(let i = 0; i < nums.length; i++){
-        minPriQue.enqueue(nums[i]);
-        if(minPriQue.size() > k){
-                minPriQue.dequeue();
-            }
+const { MinPriorityQueue } = require("@datastructures-js/priority-queue");
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let q = new MinPriorityQueue()
+    let map = new Map()
+    let ans=[]
+
+    for(let i=0;i<nums.length;i++){
+        if(map.has(nums[i])){
+            map.set(nums[i],map.get(nums[i])+1)
+        }else{
+            map.set(nums[i],1)
+        }
     }
 
-    return minPriQue.front().element
+    for (let [num, frequency] of map) {
+        q.enqueue([frequency, num]);
+        if (q.size() > k) {
+            q.dequeue();
+        }
+    }
+
+    while(q.size()>0){
+        ans.push(q.dequeue()[1])
+    }
+
+    return ans
 };
+
+console.log(topKFrequent([1,1,1,2,2,3],2))
