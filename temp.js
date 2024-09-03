a2a71129e01802c0f0e60bfe27484d4cabb7ce73
namit4saraswat <namit.and.saraswat@gmail.com>
2024-09-03 16:12:28 +0530

    739 daily temperature

--- temp.js
+++ temp.js
@@ -1,24 +1,21 @@
-const  {getRootNode, printList} =  require ("./LinkedList/createLLFromArr")
-
-
-
-var swapPairs = function(head) {
-    let one = head
-    let two = head.next
-    let newHead = head.next
-    let prevPair = null
-    while(two){
-        let three = two.next
-        two.next = one
-        one.next = three
-        prevPair = one
-        two = three.next
-        one = three
-        prevPair.next = two
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let st = []
+    st.push(temperatures[0])
+   
+    let ans = new Array(temperatures.length).fill(0)
+    for(let i=0;i<temperatures.length;i++){
+        while(st.length>0 && st[st.length-1].val<temperatures[i]){
+            let index = st.pop().idx
+            ans[index] = i - index 
+        }
+        st.push({val: temperatures[i], idx: i})
     }
-    return newHead
+
+    return ans
 };
 
-const root = getRootNode([1,3,5,7,9])
-let head = swapPairs(root)
-console.log(printList(head))
+console.log(dailyTemperatures([30]))
