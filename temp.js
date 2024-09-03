58213ec011e179b96fe2ab3345b5050e95cd5c7e
namit4saraswat <namit.and.saraswat@gmail.com>
2024-09-03 17:09:36 +0530

    merge intervals

--- temp.js
+++ temp.js
@@ -1,35 +1,26 @@
-const { MinPriorityQueue } = require("@datastructures-js/priority-queue");
-
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
+ * @param {number[][]} intervals
+ * @return {number[][]}
  */
-var topKFrequent = function(nums, k) {
-    let q = new MinPriorityQueue()
-    let map = new Map()
-    let ans=[]
+var merge = function(intervals) {
+    let ans = []
+    intervals = intervals.sort((a, b) => a[0] - b[0]);
+    let idx = -1;
 
-    for(let i=0;i<nums.length;i++){
-        if(map.has(nums[i])){
-            map.set(nums[i],map.get(nums[i])+1)
+    let end = null;
+
+    for(let i=0;i<intervals.length;i++){
+        if((end == null) || end<intervals[i][0]){
+            ans.push([intervals[i][0],intervals[i][1]])
+            end = intervals[i][1]
+            idx++
         }else{
-            map.set(nums[i],1)
+            ans[idx][1] = Math.max(intervals[i][1],end)
+            end = Math.max(intervals[i][1], end);
         }
-    }
-
-    for (let [num, frequency] of map) {
-        q.enqueue([frequency, num]);
-        if (q.size() > k) {
-            q.dequeue();
-        }
-    }
-
-    while(q.size()>0){
-        ans.push(q.dequeue()[1])
     }
 
     return ans
 };
 
-console.log(topKFrequent([1,1,1,2,2,3],2))
+console.log(merge([[2,6],[1,3],[8,10],[15,18]]))
