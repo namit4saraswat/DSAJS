b67d64bf8aa57faed44dfde91e4912b6a3572753
namit4saraswat <namit.and.saraswat@gmail.com>
2024-09-13 07:56:21 +0530

    backtracking permutation

--- temp.js
+++ temp.js
@@ -1,44 +1,29 @@
-// /**
-//  * @param {character[][]} grid
-//  * @return {number}
-//  */
-var numIslands = function(grid) {
-    let r = [-1,0,1,0]
-    let c = [0,1,0,-1]
-    let ans=0
-    function isValid(i,j){
-        if(i>=0 && i<grid.length && j>=0 && j<grid[0].length && grid[i][j]==='1' && grid[i][j]!='2'){
-            grid[i][j]='2'
-            return true
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    let ans = []
+    let arr = []
+    let l = nums.length
+    function permute(nums,ans,arr){
+        if(arr.length === l){
+            ans.push([...arr])
+            return
         }
-    }
+        for(let i=0;i<nums.length;i++){
+            arr.push(nums[i])
+            permute(nums.slice(0,i).concat(nums.slice(i+1)), ans,arr)
+            arr.pop()
 
-    function dfs(i ,j){
-        if(isValid(i,j)){
-            for(let k=0;k<r.length;k++){
-                dfs(i+r[k],j+c[k])
-            }
-            
         }
         
     }
-    
-    for(let i=0;i<grid.length;i++){
-        for(let j=0;j<grid[0].length;j++){
-            if(grid[i][j]==='1'){
-                ans++
-                dfs(i,j)
-            }
-        }
-
-    }
+    permute(nums,ans,arr,0)
 
     return ans
+    
 };
 
-console.log(numIslands([
-    ["1","1","1","1","0"],
-    ["1","1","0","1","0"],
-    ["1","1","0","0","0"],
-    ["0","0","0","0","0"]
-  ]))
+
+console.log(permute([1,2,3]))
